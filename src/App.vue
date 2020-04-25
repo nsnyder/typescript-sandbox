cf3166f848d9f5c19625de0054ceb33cad216410
Nathan Snyder <nathan@snydern.com>
2020-04-25 19:28:50 -0400

    Demonstrate that it works without the Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
   <div id="app">
     <test-me />
+    <test-me-again />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { ref } from "@vue/composition-api";
 import TestMe from "./test-me.vue";
+import TestMeAgain from "./test-me-again.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    TestMe
-  },
-  setup() {
-    const truthy = ref(true);
-    const thing = ref("I'm a thing!");
-    return {
-      thing,
-      truthy
-    };
+    TestMe,
+    TestMeAgain
   }
 });
 </script>
---
+++ src/test-me-again.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span v-if="truthy">
+      This is all ok. ->
+      {{ thing }}
+    </span>
+
+    <span v-if="truthy.test">
+      This correctly errors!
+    </span>
+
+    <span v-if="truthier">
+      This is wrong. ->
+      {{ thingo }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TestMe",
+
+  data() {
+    return {
+      truthy: true,
+      thing: "I'm a thing!"
+    };
+  }
+};
+</script>
