ce538c677fab597db2e62e3a910e48661fa8adf2
Nathan Snyder <nathan@snydern.com>
2020-04-25 19:06:35 -0400

    Demonstrate Vetur issues

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <test-me />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "@vue/composition-api";
+import TestMe from "./test-me.vue";
 
 export default Vue.extend({
   name: "App",
+
   components: {
-    HelloWorld
+    TestMe
+  },
+  setup() {
+    const truthy = ref(true);
+    const thing = ref("I'm a thing!");
+    return {
+      thing,
+      truthy
+    };
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/test-me.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span v-if="truthy">
+      This is all ok. ->
+      {{ thing }}
+    </span>
+
+    <span v-if="truthy.test">
+      This correctly errors!
+    </span>
+
+    <span v-if="truthier">
+      This is wrong. ->
+      {{ thingo }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "TestMe",
+
+  setup() {
+    const truthy = ref(true);
+    const thing = ref("I'm a thing!");
+    return {
+      thing,
+      truthy
+    };
+  }
+});
+</script>
